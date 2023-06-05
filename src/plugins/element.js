1e923665aa4881acf663f6ca7ad4eafdb12be581
okok111 <1for.rails@gmail.com>
2023-06-06 07:17:12 +0900

    vue element削除

--- src/plugins/element.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/ja'
-
-const app = createApp(/* Your App Component */);
-app.use(Element, { locale });
-app.mount(/* Your App Mount Element */);
-
